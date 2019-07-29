1cdb0a3fe39453e06d80737246cf2ed249ebb589
oril-software <developer@orilinc.com>
2019-07-29 15:41:52 +0300

    Updated config and handler

--- handler.js
+++ handler.js
@@ -1,15 +1,25 @@
 const AWS = require('aws-sdk');
 
-module.exports.handler = async (event) => {
+module.exports.getAll = async (event) => {
     const docClient = new AWS.DynamoDB.DocumentClient();
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
+        const response = {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            }
+        };
         docClient.scan({
             TableName: 'Cars',
         }, (err, data) => {
             if (err) {
-                reject(err);
+                response.statusCode = 500;
+                response.body = JSON.stringify(err);
+            } else {
+                response.statusCode = 200;
+                response.body = JSON.stringify(data.Items);
             }
-            resolve(data.Items);
+            resolve(response);
         });
     });
 };
